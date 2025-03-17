1948869d89740b1b2c5ef99ddd7b7e25a65ffbd6
bala-slaylewks <bala.code01@gmail.com>
2025-03-17 15:18:35 +0530

    added authtoken in routes for trips

--- src/routes/tripRoutes.js
+++ src/routes/tripRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const tripController = require('../controllers/trips/tripsController');
+const authToken = require("../middleware/authToken");
 
 // Define trip-related routes
 router.post('/create', tripController.createTrip);
@@ -11,5 +12,15 @@ router.get('/:id', tripController.getTripById);
 router.get('/user/:user_id', tripController.getTripsByUserId);
 router.put('/:id', tripController.updateTrip);
 router.delete('/:id', tripController.deleteTrip);
+
+
+/// unccomment these lines for authentication code
+
+// router.post('/create', authToken, tripController.createTrip);
+// router.get('/list',authToken, tripController.getAllTrips);
+// router.get('/:id',authToken, tripController.getTripById);
+// router.get('/user/:user_id',authToken, tripController.getTripsByUserId);
+// router.put('/:id',authToken, tripController.updateTrip);
+// router.delete('/:id',authToken, tripController.deleteTrip);
 
 module.exports = router;
